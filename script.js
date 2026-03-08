1b6f63f2206d1a77bd3d0af4fa2ca32bf1388358
JERIN SEBASTIAN <jerinsebastian@JERINs-MacBook-Air.local>
2026-03-08 12:01:29 +0530

    fontandcolorchange

--- script.js
+++ script.js
@@ -72,12 +72,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.querySelector('.nav-container');
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
-            navbar.style.background = 'rgba(255, 255, 255, 0.9)';
-            navbar.style.borderBottom = '1px solid rgba(0, 0, 0, 0.05)';
+            navbar.style.background = 'rgba(255, 255, 255, 0.4)';
+            navbar.style.borderBottom = '1px solid rgba(255, 255, 255, 0.3)';
             navbar.style.boxShadow = '0 10px 30px rgba(0, 0, 0, 0.05)';
         } else {
-            navbar.style.background = 'rgba(255, 255, 255, 0.7)';
-            navbar.style.borderBottom = '1px solid rgba(0, 0, 0, 0.05)';
+            navbar.style.background = 'rgba(255, 255, 255, 0.3)';
+            navbar.style.borderBottom = '1px solid rgba(255, 255, 255, 0.2)';
             navbar.style.boxShadow = 'none';
         }
     });
